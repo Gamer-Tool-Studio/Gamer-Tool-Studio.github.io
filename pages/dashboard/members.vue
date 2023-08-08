d20681aabaaca1245982b6c15e81b738ab1ae8c9
0xVallados <0xvallados@gmail.com>
2023-08-08 21:10:19 +0100

    added usage and members pages

---
+++ pages/dashboard/members.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="10">
+        <h1>Members</h1>
+      </v-col>
+      <v-col cols="2">
+        <button class="button" type="submit">Invite members</button>
+      </v-col>
+      <v-col v-for="member in members">
+        <dashboard-member-card :member="member"> </dashboard-member-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script setup>
+import { storeToRefs } from "pinia";
+import { useUserStore } from "@/store/user";
+const store = useUserStore();
+
+const { members } = storeToRefs(store);
+</script>
---
+++ pages/dashboard/usage.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1>Usage</h1>
+      </v-col>
+      <v-col>
+        <h5>
+          Below you'll find a summary of API usage for your organization. All
+          dates and times are UTC-based, and data may be delayed up to 5
+          minutes.
+        </h5>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script setup></script>
