607e22e4b7236cf08aa7f080ada4c4167e79c060
0xVallados <0xvallados@gmail.com>
2023-08-25 12:51:36 +0100

    added pages

---
+++ pages/dashboard/billing/overview.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1>Overview</h1>
+      </v-col>
+      <v-col>
+        <v-form>
+          <h4>Organization name</h4>
+          <label>
+            Human-friendly label for your organization, shown in user interfaces
+          </label>
+          <h4>Organization ID</h4>
+          <label>
+            Identifier for this organization sometimes used in API requests
+          </label>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+useHead({
+  title: "Overview ",
+});
+</script>
