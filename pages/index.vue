0e04665fbed39f2f2eea0c7fcc3c785b4eb3ecad
0xVallados <0xvallados@gmail.com>
2023-09-18 15:06:43 +0100

    rework typing method

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,7 @@
   <v-container class="lp">
     <v-row>
       <v-col cols="12" class="intro-section">
-        <h1>
-          Dialog Craft GPT - The Tool To Power Your Game Narratives with AI
-        </h1>
+        <h1>Dialog Craft GPT - The Tool To Power Your Game Narratives with AI</h1>
         <div class="intro-cta">
           <button class="button">Start Free Trial Now</button>
           <p>View our documentation</p>
@@ -28,42 +26,38 @@
     <v-row class="features">
       <v-col>
         <h2 class="page-headers">
-          Take your storytelling to new heights with a game development tool to
-          revolutionize the way you create character dialogues for your games.
-          Craft immersive conversations efortlessly with the power of Chat GPT
-          AI technology
+          Take your storytelling to new heights with a game development tool to revolutionize the way you create
+          character dialogues for your games. Craft immersive conversations efortlessly with the power of Chat GPT AI
+          technology
         </h2>
       </v-col>
       <div class="feature-boxes">
         <div class="feature-box">
           <h3>Open interactions</h3>
           <p>
-            Leverage the capabilities of GPT-3 and enable players to input
-            their own text to interact with characters and open the possibilities
-            within to your game to the maximum.
+            Leverage the capabilities of GPT-3 and enable players to input their own text to interact with characters
+            and open the possibilities within to your game to the maximum.
           </p>
         </div>
         <div class="feature-box">
           <h3>AI-Powered Characters</h3>
           <p>
-            Build immersive character backstories and customize the way they
-            respond to players. Make chat GPT a friend or a foe in your game
-            quests.
+            Build immersive character backstories and customize the way they respond to players. Make chat GPT a friend
+            or a foe in your game quests.
           </p>
         </div>
         <div class="feature-box">
           <h3>Intuitive Interface</h3>
           <p>
-            DialogCraftGPT plugin requires no programming experience to setup
-            and use from within your favourite game engine.
+            DialogCraftGPT plugin requires no programming experience to setup and use from within your favourite game
+            engine.
           </p>
         </div>
         <div class="feature-box">
           <h3>Secure and Private</h3>
           <p>
-            Rest assured that your sensitive game content and storylines are
-            safeguarded. We follow strict privacy standards to protect your
-            intellectual property.
+            Rest assured that your sensitive game content and storylines are safeguarded. We follow strict privacy
+            standards to protect your intellectual property.
           </p>
         </div>
       </div>
@@ -71,10 +65,12 @@
     <v-row class="get-started" id="sectionToLinkToApi">
       <v-col>
         <h2 class="page-headers">
-          Create seamless AI characters to interact with players and users from any Frontend environment with Dialog Craft GPT library</h2>
-        <pre class="code-display">
+          Create seamless AI characters to interact with players and users from any Frontend environment with Dialog
+          Craft GPT library
+        </h2>
+        <div class="code-style">
+          <pre class="code-display">
           <code>
-            <div class="code-style">
             npm install dialogcrafgpt
             curl https://api.gamertoolstudio.com/dialogcraftgpt/v1/ 
             -H "Content-Type: application/json" \
@@ -92,55 +88,54 @@
               "userInput": "Hey there, where am i?,
               "chatHistory": []
             }'
-            <div class="typed-display">"GPT Wizard response": <span class="typed-message" ref="textElement"></span></div>
-          </div>
           </code>
         </pre>
+          <div class="typed-display">
+            "GPT Wizard response":
+            <span class="typed-message">
+              {{ responseGPT }}
+            </span>
+          </div>
+        </div>
       </v-col>
       <v-col cols="12" class="button-display">
         <button class="button">View API Reference</button>
       </v-col>
       <v-col cols="12" id="sectionToLinkToPlugin">
         <h2 class="page-headers">
-          Add Dialog Craft GPT Plugin to your favourite game engine today and
-          start harnessing the power of AI-generated dialogs with ease
+          Add Dialog Craft GPT Plugin to your favourite game engine today and start harnessing the power of AI-generated
+          dialogs with ease
         </h2>
       </v-col>
-        <v-col class="plugin-explain">
-          <ol class="how-to-start">
-            <li>
-              Sign up for a trial account or select a suitable paid plan.
-            </li>
-            <li>
-              Install the DialogCraftGPT plugin in your preferred game engine. <a href="/download-plugin">Download Plugin file</a>.
-            </li>
-            <li>
-              Define personality traits, provide context about game events and
-              establish guidelines for your character dialogues with seamless
-              plugin commands.
-            </li>
-            <li>
-              Harness the power of Chat GPT AI to generate dynamic and immersive
-              conversations.
-            </li>
-            <li>Integrate the generated dialogues into your game seamlessly.</li>
-          </ol>
-        </v-col>
-        <v-col cols="6" class="engines-available">
-          <div class="engine-logo-container">
-            <img class="engine-logo" src="images/RMMZ.png" />
-          </div>
-          <div class="engine-logo-container">
-            <img class="engine-logo" src="images/RMMV.png" />
-          </div>
-        </v-col>
-
+      <v-col class="plugin-explain">
+        <ol class="how-to-start">
+          <li>Sign up for a trial account or select a suitable paid plan.</li>
+          <li>
+            Install the DialogCraftGPT plugin in your preferred game engine.
+            <a href="/download-plugin">Download Plugin file</a>.
+          </li>
+          <li>
+            Define personality traits, provide context about game events and establish guidelines for your character
+            dialogues with seamless plugin commands.
+          </li>
+          <li>Harness the power of Chat GPT AI to generate dynamic and immersive conversations.</li>
+          <li>Integrate the generated dialogues into your game seamlessly.</li>
+        </ol>
+      </v-col>
+      <v-col cols="6" class="engines-available">
+        <div class="engine-logo-container">
+          <img class="engine-logo" src="images/RMMZ.png" />
+        </div>
+        <div class="engine-logo-container">
+          <img class="engine-logo" src="images/RMMV.png" />
+        </div>
+      </v-col>
     </v-row>
-      <section>
+    <section>
       <div class="start-trial">
         <h2 class="page-headers">
-          Are you ready to take your game development to the next level with the
-          ultimate AI-Powered character building tool?
+          Are you ready to take your game development to the next level with the ultimate AI-Powered character building
+          tool?
         </h2>
         <br />
         <button class="button">Start Free Trial Now</button>
@@ -149,54 +144,52 @@
   </v-container>
 </template>
 <script lang="ts" setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted } from 'vue';
 
-const typedMessage = ref(""); // Initialize an empty message
 const messageToType =
   "Hey there, you seem kinda lost huh? you're exactly where you should be but soon to be the worst of the places to ever been. The beginning may be the end but for you still both.";
-const textElement = ref(null); // Reference to the text element
 
 // Typing animation
-const typeMessage = async () => {
-  let shouldRestart = true;
-  let currentMessage = ""; // Store the current message
+let intervalTypeMessage: any;
+const responseGPT = ref('');
+function typeMessage(textToType: string, typingSpeed: number) {
+  let currentIndex = 0;
 
-  while (true) {
-    for (let i = 0; i < messageToType.length; i++) {
-      currentMessage += messageToType[i];
-      textElement.value.textContent = currentMessage; // Update the text element
-      await new Promise((resolve) => setTimeout(resolve, 50)); // Adjust typing speed here
-    }
+  function typeNextCharacter() {
+    if (currentIndex < textToType.length) {
+      const currentCharacter = textToType.charAt(currentIndex);
+      responseGPT.value += currentCharacter;
+      currentIndex++;
 
-    // Wait for 2 seconds before restarting
-    if (shouldRestart) {
-      shouldRestart = false;
-      await new Promise((resolve) => setTimeout(resolve, 2000)); // 2 seconds delay
-      currentMessage = ""; // Clear the current message
-      textElement.value.textContent = ""; // Clear the text element
-    } else {
-      // Clear the typed message and start over
-      currentMessage = "";
-      textElement.value.textContent = "";
-      shouldRestart = true;
+      setTimeout(typeNextCharacter, typingSpeed);
     }
   }
-};
+  typeNextCharacter();
+}
 
 // Start typing animation when the component is mounted
 onMounted(() => {
-  typeMessage();
+  const typingSpeed = 50;
+  typeMessage(messageToType, typingSpeed);
+  intervalTypeMessage = setInterval(async () => {
+    if (responseGPT.value == messageToType) {
+      responseGPT.value = '';
+      typeMessage(messageToType, typingSpeed);
+    }
+  }, 3000);
 });
 
+onBeforeRouteLeave(() => clearInterval(intervalTypeMessage));
+
 definePageMeta({
-  layout: "default",
+  layout: 'default',
 });
 
 useHead({
-  title: "Home Page",
+  title: 'Home Page',
 });
 </script>
 
 <style lang="scss" scoped>
-@import "../assets/lp_styles.scss";
+@import '../assets/lp_styles.scss';
 </style>
