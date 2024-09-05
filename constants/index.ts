ba74b7d0f9cb86eae802a72bacaf39c31d2bc78b
0xVallados <0xvallados@gmail.com>
2024-09-05 13:08:08 +0100

    Add method to calculate votes percentage

--- constants/index.ts
+++ constants/index.ts
@@ -6,3 +6,49 @@ export const BASE_URL = _process.server
     ? `http://localhost:3002/api/${apiVersion}/`
     : `https://example.com/api/${apiVersion}/`
   : null
+
+export const NFTS_LIST = [
+  {
+    id: 1,
+    name: 'The Wife',
+    description: 'The outspoken wife of Mr.Hamilton. Does the victim\'s life partner conceal his darkest secrets?',
+    src: '/images/wife.png',
+  },
+  {
+    id: 2,
+    name: 'The Butler',
+    description: 'A refined gentleman in whom Mr. Hamilton placed the highest trust. Was it misplaced?',
+    src: '/images/butler.png',
+  },
+  {
+    id: 3,
+    name: 'The Maid',
+    description: 'A shy and caring servant who might know more about her masters\' life than they know.',
+    src: '/images/maid.png',
+  },
+  {
+    id: 4,
+    name: 'The Gardener',
+    description: 'A simple working man that seems to only care about plants and flowers. Or does he?',
+    src: '/images/gardener.png',
+  },
+  {
+    id: 5,
+    name: 'The Cook',
+    description: 'An extravagant cooking master who\'s been in the family since she was a child? Is there a catch?',
+    src: '/images/cook.png',
+  },
+  {
+    id: 6,
+    name: 'The Journalist',
+    description: 'A rising reporting star investigating Mr.Hamilton\'s latest scandal. Is he involved?',
+    src: '/images/journalist.png',
+  },
+  {
+    id: 7,
+    name: 'The Businessman',
+    description: 'A rude influential businessman with life-long dealings with Mr.Hamilton. Have things gone wrong?',
+    src: '/images/businessman.png',
+  },
+
+]
