166c66f3b6ecbbd2ca363b4ae8dc43fb38b52dc5
S3mz <ourtube3@protonmail.com>
2024-11-22 14:58:21 +0000

    fix login and signup styles

---
+++ components/modal/trialModal.vue
@@ -0,0 +1,88 @@
+<script>
+// import DialogCraftGPTLib from 'dialogcraftgpt-lib'
+
+export default {
+  name: 'trialModal',
+  components: {},
+  emits: ['close'],
+  data() {
+    return { chatGPTText: '' }
+  },
+
+  methods: {
+    close() {
+      this.$emit('close')
+    },
+  },
+
+}
+</script>
+
+<template>
+  <transition name="modal-fade">
+    <div v-show="true" class="modal-backdrop" @click="close">
+      <div
+        class="modal"
+        role="dialog"
+        aria-labelledby="modalTitle"
+        aria-describedby="modalDescription"
+        @click.stop
+      >
+        <header class="modal-header">
+          <slot name="header">
+            <h1>Cancel plan</h1>
+            <button
+              type="button"
+              class="btn-close"
+              aria-label="Close modal"
+              @click="close"
+            >
+              <img src="">
+            </button>
+          </slot>
+        </header>
+        <section id="modalDescription" class="modal-body">
+          <slot name="body">
+            <v-col cols="12" class="text-section">
+              <p>Are you sure you want to cancel your payment plan?</p>
+            </v-col>
+            <v-col cols="12" class="text-section">
+              <p>
+                You'll still have access until the end of this calendar month,
+                after which we'll send you a final invoice and begin rejecting your API requests.
+              </p>
+            </v-col>
+            <v-col cols="12" class="footer-section">
+              <button class="cancel-button" @click="close">
+                Nevermind
+              </button>
+              <button class="button red">
+                Cancel Plan
+              </button>
+            </v-col>
+          </slot>
+        </section>
+      </div>
+    </div>
+  </transition>
+</template>
+
+  <style lang="scss">
+.text-section p {
+    font-size: 16px !important;
+}
+
+.cancel-button:hover {
+  background-color: #d9d9e3;
+}
+
+  .red {
+    background-color:#EF4146 !important;
+    color:#fff;
+  }
+
+  .red:hover {
+    background-color:#c23539 !important;
+    color:#fff;
+  }
+</style>
